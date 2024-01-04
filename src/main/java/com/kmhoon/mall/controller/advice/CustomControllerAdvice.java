24ec2e758193088efa913b35e1c16b3c91d01e05
K-MHOON <azmria117@gmail.com>
2024-01-04 19:47:48 +0900

    feat : ExceptionAdvice 추가

---
+++ src/main/java/com/kmhoon/mall/controller/advice/CustomControllerAdvice.java
@@ -0,0 +1,26 @@
+package com.kmhoon.mall.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<?> notExist(NoSuchElementException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("msg", message));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<?> handleIllegalArgumentException(MethodArgumentNotValidException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", message));
+    }
+}
